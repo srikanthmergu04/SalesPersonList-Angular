3918cf02b60cc6cc21f48a0b19cfa657804fa075
Srikanth Mergu <srikanth.mergu@pegs.com>
2020-04-19 17:56:18 +0530

    initial commit

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  constructor() { }
+
+  salesPersonList:SalesPerson[] = [
+    new SalesPerson('rakesh','kumar','rakesh@gmail.com',1000),
+    new SalesPerson('captain','america','captain@gmail.com',2000),
+    new SalesPerson('Tony','Stark','tony@gmail.com',1000),
+    new SalesPerson('hulk','angry','hulk@gmail.com',2000),
+  ];
+
+
+
+  ngOnInit(): void {
+  }
+
+}
